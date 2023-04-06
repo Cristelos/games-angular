b7167377479c0147ab17975c0df200a982eb8677
Cristelos <cristelos@msn.com>
2023-04-06 16:20:43 +0200

    trying to print the images in the hit game

--- src/app/pages/hit/hit.component.ts
+++ src/app/pages/hit/hit.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hit',
   templateUrl: './hit.component.html',
   styleUrls: ['./hit.component.scss']
 })
-export class HitComponent {
+export class HitComponent implements OnInit{
+
+  score: number;
+  tiempo: number;
+  squares: any[];
+  squareClasses: any;
+  // bgImage = "assets/exercise-2/bg.jpg";
+  // moleImage = "assets/exercise-2/mole.png";
+
+  constructor(){
+    this.score = 0;
+    this.tiempo = 0;
+    this.squares = [];
+    this.squareClasses = {
+      'b-square': true,
+      'b-mole': false
+    };
+  }
+
+  ngOnInit(): void {
+   this.crearTabla();
+ }
+
+ crearTabla(){
+  for (let i = 0; i<9 ; i++) {
+    this.squares.push({ isMole: false })
+  }
+ }
+
+ moverTopo(){
+  const index = Math.floor(Math.random() * 9);
+  this.squares.forEach((square, i) => {
+    square.isMole = (i === index);
+  })
+}
+
 
 }
